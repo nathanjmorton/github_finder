59a052ea0b3f2a3bf617ad5a3df3bce9e0af668d
Nathan Morton <nathan.j.morton@gmail.com>
2020-12-23 08:09:02 -0500

    this

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+    alert: null 
+  }
+
+  // search Github users
+  searchUsers = async text => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );    
+    
+    this.setState({ users: res.data.items, loading: false });
+  }
+
+  getUser = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );    
+    
+    this.setState({ users: res.data.items, loading: false });
+  
+  }
+
+  clearUsers = e => {
+    this.setState({ users: [], loading: false });
+  }
+  
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type }});
+    setTimeout(() => this.setState({ alert: null}), 2000);
+  }
+  
+  render() {
+    const { users, loading } = this.state;
+
+    return (
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route 
+                exact 
+                path='/' 
+                render={props => (
+                  <Fragment>
+                    <Search 
+                      searchUsers={this.searchUsers} 
+                      clearUsers={this.clearUsers} 
+                      showClear={users.length > 0 ? true : false} 
+                      setAlert={this.setAlert}
+                    /> 
+                    <Users 
+                      loading={loading} 
+                      users={users} 
+                    /> 
+                  </Fragment>
+                )} 
+              />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
