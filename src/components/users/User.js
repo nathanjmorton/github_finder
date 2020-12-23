97f12bbea67ae7c914ad7a35859c8d9f46e703f0
Nathan Morton <nathan.j.morton@gmail.com>
2020-12-23 11:09:50 -0500

    this

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class User extends Component {
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+  }
+
   render() {
     return (
       <div>
