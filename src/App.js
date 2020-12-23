147b780887d3cb69c3bb798bdf297fb2abc719c8
Nathan Morton <nathan.j.morton@gmail.com>
2020-12-23 13:25:19 -0500

    this

--- src/App.js
+++ src/App.js
@@ -13,8 +13,9 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [], 
     loading: false,
-    alert: null 
+    alert: null,
   }
 
   // search Github users
@@ -36,7 +37,16 @@ class App extends Component {
     );    
 
     this.setState({ user: res.data, loading: false });
-  
+  }
+
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );    
+
+    this.setState({ repos: res.data, loading: false });
   }
 
   clearUsers = e => {
@@ -78,7 +88,12 @@ class App extends Component {
               />
               <Route exact path='/about' component={About} />
               <Route exact path='/user/:login' render={props => (
-                <User { ...props } getUser={this.getUser} user={user} loading={loading} />
+                <User 
+                  { ...props } 
+                  getUser={this.getUser} 
+                  user={user} 
+                  loading={loading} 
+                />
               )} />
             </Switch>
           </div>
