f558d96f65a42148f9454e44da65f77567df533d
Nathan Morton <nathan.j.morton@gmail.com>
2020-12-24 22:10:11 -0500

    this

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,17 +1,20 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect, Fragment, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
-const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
-  
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext);
+  const { user, loading, getUser, repos, getUserRepos } = githubContext;
+
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
     // eslint-disable-next-line
   }, []);
   
+
   const {
     name,
     company,
@@ -84,14 +87,6 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
       <Repos repos={repos} />
     </Fragment>
   );
-}
-
-User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired
 }
 
 export default User;
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -17,22 +17,42 @@ const GithubState = props => {
     repos: [],
     loading: false
   }
-
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
   // search Github users
   const searchUsers = async text => {
     setLoading();
-
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );    
-    
     dispatch({
       type: SEARCH_USERS,
       payload: res.data.items 
     })
   }
+
+  const getUser = async username => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );    
+    dispatch({
+      type: GET_USER,
+      payload: res.data 
+    });
+  }
+
+  const getUserRepos = async username => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );    
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    });
+  }
+
 
   const clearUsers = () => dispatch({ type: CLEAR_USERS });
   
@@ -46,7 +66,9 @@ const GithubState = props => {
       repos:state.repos,
       loading: state.loading,
       searchUsers,
-      clearUsers
+      clearUsers,
+      getUser,
+      getUserRepos 
     }}
   >
     {props.children}
